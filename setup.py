d815133e878bbcb03f333abd730247919a28f824
nalourie <nalourie@gmail.com>
2014-08-21 03:07:20 -0400

    packaged up library, adding setup.py etc..., submitted to PyPI

---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-macros',
+    version='0.1',
+    packages=['macros'],
+    include_package_data=True,
+    install_requires='django >= 1.6',
+    license='MIT License',
+    description='A Django template tag library for repeating blocks tags and creating in template macros.',
+    long_description=README,
+    maintainer="Nick Lourie",
+    maintainer_email="developer.nick@kozbox.com",
+    keywords = "django repeat macros macro templatetags",
+    url='https://github.com/nalourie/django-macros',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
+
